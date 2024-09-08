ee03f7c3d60be4a0f64dab39c21f8378d9fb9952
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-08 12:41:10 +0500

    Que 22: Intentional Error

---
+++ Main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Que 22: Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one 
+// happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before
+// closing the program.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Creating Array
+let newArray = ["Will smith", "James Bond", "Tom cruise"];
+// Accessing Array by Index
+console.log(newArray[4]);
+// Make sure you correct the error before closing the program.
+// Correct the Error
+console.log(newArray);
---
+++ Main.ts
@@ -0,0 +1,13 @@
+// Que 22: Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one 
+// happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before
+// closing the program.
+
+// Creating Array
+let newArray: string[] = ["Will smith" , "James Bond" , "Tom cruise"];
+
+// Accessing Array by Index
+console.log(newArray[4]);
+
+// Make sure you correct the error before closing the program.
+// Correct the Error
+console.log(newArray);
